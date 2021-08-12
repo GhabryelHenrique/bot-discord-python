5ca93ab83358c0aff24f7945fb87c926bd2505db
Ghabryel <86971182+GhabryelHenrique@users.noreply.github.com>
2021-08-12 20:04:00 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 #Import Libs
-
+from re import A
 import discord
 import feedparser
 from discord.ext import commands, tasks
 from bs4 import BeautifulSoup
 import credentials
+import urllib.request 
 
 #Define prefix to Bot
 client = commands.Bot(command_prefix = '!', case_insensitive = True)
@@ -27,40 +28,34 @@ class MyClient(discord.Client):
         super().__init__(*args, **kwargs)
 
         # an attribute we can access from our task
-        self.counter = 0
+        self.counter = 1
 
         # start the task to run in the background
         self.noticias.start()
 
     async def on_ready(self):
-        print('Estamos online!')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+        channel1 = self.get_channel(credentials.log)
+        await channel1.send('Estamos online!')
+
 
     @tasks.loop(minutes=60) # task runs every X seconds
     async def noticias(self):
         channel = self.get_channel(credentials.canal) # channel ID 
         self.counter += 1
-
-        if entry.id != nnoticia:
-            await channel.send('---------')
-            await channel.send(data)
-            await channel.send(titulo)
-            await channel.send(soup.get_text())
-            await channel.send('**------Link da noticia----**')
-            await channel.send(link)
-
-        else:
-            print('Sem novas noticias')
+        await channel.send('**---------**')
+        await channel.send(data)
+        await channel.send(titulo)
+        await channel.send('-')
+        await channel.send(soup.get_text())
+        await channel.send('**----Link da noticia----**')
+        await channel.send(link)
+        await channel.send(self.counter)
 
      
     @noticias.before_loop
     async def before_my_task(self):
         await self.wait_until_ready() # wait for the bot to login
 
-#Whenever the RSS id changes it will be relocated to the variable
-nnoticia = entry.id
 
 #Start this Bot
 client = MyClient()
