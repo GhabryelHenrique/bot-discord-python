1a6e0ff40a46b97ae004cd6ce9d3a859499bef19
Ghabryel <86971182+GhabryelHenrique@users.noreply.github.com>
2021-08-15 21:41:56 -0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,3 @@
-rss = 'https://uemg.br/noticias-uemg?format=feed&amp;type=rss'
-token = 'ODY5MzMwOTUyODM5NjQ3Mjky.YP8pig.krIRLuZ7EusB-VZhzsxLKOfQn1k'
-canal = 856969430490415145
-log = 861217407141937182
-aulas = 856946114217246721
+rss = #URL to RSS 
+token = #Token Bot
+canal = # Id Channel
