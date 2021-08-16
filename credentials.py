fb6a18c46b38297a95326f85e7b80c6bf671ab06
Ghabryel <86971182+GhabryelHenrique@users.noreply.github.com>
2021-08-15 21:41:06 -0300

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,5 @@
+rss = 'https://uemg.br/noticias-uemg?format=feed&amp;type=rss'
+token = 'ODY5MzMwOTUyODM5NjQ3Mjky.YP8pig.krIRLuZ7EusB-VZhzsxLKOfQn1k'
+canal = 856969430490415145
+log = 861217407141937182
+aulas = 856946114217246721
