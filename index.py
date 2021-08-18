0cf292e94903046a102b9f053eeb8bccd250cd2c
Ghabryel <86971182+GhabryelHenrique@users.noreply.github.com>
2021-08-18 07:42:23 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,24 +1,14 @@
 #Import Libs
 import discord
-import feedparser
 from discord.ext import commands, tasks
-from bs4 import BeautifulSoup
 import credentials
+import sopa
+
+intents = discord.Intents.default()
+intents.members = True
 
 #Define prefix to Bot
-client = commands.Bot(command_prefix = '!', case_insensitive = True)
-
-#Define RSS feed
-NewsFeed = feedparser.parse(credentials.rss)
-entry = NewsFeed.entries[0]
-
-#Transform HTML to Normal Language
-soup = BeautifulSoup(markup=entry.summary, features="lxml") 
-
-#Defining variables its optional
-titulo = entry.title
-data = entry.published
-link = entry.link
+client = commands.Bot(command_prefix = '!', case_insensitive = True, intents=intents)
 
 #Crate a loop
 class MyClient(discord.Client):
@@ -35,25 +25,32 @@ class MyClient(discord.Client):
         channel1 = self.get_channel(credentials.log)
         await channel1.send('Estamos online!')
 
-
     @tasks.loop(minutes=60) # task runs every X seconds
     async def noticias(self):
-        channel = self.get_channel(credentials.canal) # channel ID 
-        self.counter += 1
+        channel = self.get_channel(credentials.canal)
+        channel1 = self.get_channel(credentials.log) # channel ID 
         await channel.send('**---------**')
-        await channel.send(data)
-        await channel.send(titulo)
+        await channel.send(sopa.data)
+        await channel.send(sopa.titulo)
         await channel.send('-')
-        await channel.send(soup.get_text())
+        await channel.send(sopa.texto)
         await channel.send('**----Link da noticia----**')
-        await channel.send(link)
-        await channel.send(self.counter)
-
+        await channel.send(sopa.link)
+        await channel1.send('Noticia Enviada!')
+        print('Noticia Enviada!')
      
     @noticias.before_loop
     async def before_my_task(self):
         await self.wait_until_ready() # wait for the bot to login
 
+    
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!Hellow'):
+            await message.reply('Hellow!', mention_author=True)
 
 #Start this Bot
 client = MyClient()
